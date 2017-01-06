81fa042406bedc5de386ec825c99b88990e8b289
Darion Freeman <darionfman@gmail.com>
2017-01-06 16:10:05 -0600

    Adds in commented out examples for redux files.

---
+++ client/actions/action.js
@@ -0,0 +1,14 @@
+//This is an example of a redux action with axios
+
+//import axios from 'axios'
+// export function getUser() {
+//   return dispatch =>
+//     axios.get('/api/user')
+//     .then(res => res)
+//     .then(user =>
+//       dispatch({
+//         type: 'GET_USER',
+//         payload: user
+//       })
+//     )
+// }
